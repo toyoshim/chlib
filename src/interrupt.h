b7c926679f1421fdcb578aeab174ffab1bd71744
Takashi Toyoshima <toyoshim@gmail.com>
2023-10-18 03:25:53 +0900

    update interrupt.h

--- src/interrupt.h
+++ src/interrupt.h
@@ -6,10 +6,20 @@
 #define __interrupt_h__
 
 enum {
+  INT_NO_INT0 = 0,
   INT_NO_TMR0 = 1,
+  INT_NO_INT1 = 2,
+  INT_NO_TMR1 = 3,
+  INT_NO_UART = 4,
+  INT_NO_TMR2 = 5,
+  INT_NO_SPI0 = 6,
   INT_NO_TMR3 = 7,
   INT_NO_USB = 8,
+  INT_NO_ADC = 9,
   INT_NO_UART1 = 10,
+  INT_NO_PWM1 = 11,
+  INT_NO_GPIO = 12,
+  INT_NO_WDOG = 13,
 };
 
 #endif  // __interrupt_h__
